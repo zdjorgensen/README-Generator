cec70905bd5e927c8ca7f6adc6235cda32bdd883
Zach <z.d.jorgensen@gmail.com>
2022-08-13 01:49:30 -0600

    init

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,51 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = requre('./utils/generateMarkdown.js');
+
 // TODO: Create an array of questions for user input
 const questions = [];
+
+inquirer.prompt([ //Testing
+   {
+    type: 'input',
+    message: 'What is the project title?',
+    name: 'title',
+   },
+   {
+    type: 'input',
+    message: 'Enter the description of the project.',
+    name: 'description',
+   },
+   {
+    type: 'input',
+    message: 'Enter the description of the project.',
+    name: 'description',
+   },
+   {
+    type: 'input',
+    message: 'Enter the description of the project.',
+    name: 'description',
+   },
+   {
+    type: 'input',
+    message: 'Enter the description of the project.',
+    name: 'description',
+   },
+   {
+    type: 'input',
+    message: 'Enter the description of the project.',
+    name: 'description',
+   },
+   {
+    type: 'input',
+    message: 'Enter the description of the project.',
+    name: 'description',
+   }
+]).then(answer => {
+    console.log(answer);
+})
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
